f22030ac0726e56babb2116bd72d7a455d762262
axa083 <99968413+axa083@users.noreply.github.com>
2022-08-27 22:50:48 +0100

    Update streamlit_app.py
    edited title, added new heading for smoothies

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('The Healthy Diner')
 streamlit.header('Breakfast Best Hits 🙏')
 streamlit.text('MEXICAN OMELETTE (V) 🇲🇽')
 streamlit.text('SWEET POTATO ROSTI (V/GF) 🍠')
 streamlit.text('SALMON FISH CAKE 🍥')
 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
