0c9a67ca0b6d1e0b7746c004bbeb1449f9f4bfb0
axa083 <99968413+axa083@users.noreply.github.com>
2022-08-27 22:49:44 +0100

    Update streamlit_app.py
    Edited header, added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Breakfast')
-streamlit.text('MEXICAN OMELETTE (V)')
-streamlit.text('SWEET POTATO ROSTI (V/GF)')
-streamlit.text('SALMON FISH CAKE')
+streamlit.header('Breakfast Best Hits 🙏')
+streamlit.text('MEXICAN OMELETTE (V) 🇲🇽')
+streamlit.text('SWEET POTATO ROSTI (V/GF) 🍠')
+streamlit.text('SALMON FISH CAKE 🍥')
 
 
