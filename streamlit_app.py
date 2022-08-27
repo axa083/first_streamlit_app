c8e0d40ec74d81ad959693dec6f0d538ae680fb1
axa083 <99968413+axa083@users.noreply.github.com>
2022-08-27 22:40:10 +0100

    Update streamlit_app.py
    added breakfast items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.body('Breakfast')
+streamlit.body('MEXICAN OMELETTE (V)')
+streamlit.body('SWEET POTATO ROSTI (V/GF)')
+streamlit.body('SALMON FISH CAKE')
+
+
