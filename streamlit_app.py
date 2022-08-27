a04b0ef8f7dfee28130778078d468b48c0359294
axa083 <99968413+axa083@users.noreply.github.com>
2022-08-27 23:12:26 +0100

    Update streamlit_app.py
    Filter the Table Data
    
    We'll ask our app to put the list of selected fruits into a variable called fruits_selected. Then, we'll ask our app to use the fruits in our fruits_selected list to pull rows from the full data set (and assign that data to a variable called fruits_to_show). Finally, we'll ask the app to use the data in fruits_to_show in the dataframe it displays on the page.

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,8 +14,10 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #add picklist for fruit
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
+fruit_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+
 
 #add table with fruit options
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
                   
