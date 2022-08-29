b0c260cd0cfadc28d7d5192a8e003d5edfa91ffc
axa083 <99968413+axa083@users.noreply.github.com>
2022-08-29 14:35:12 +0100

    Update streamlit_app.py
    imported requests

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,4 +25,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response.json())
                   
